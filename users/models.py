29d79d2bc2d52c28ef924e616b2d11e5345fccaa
khomyakov362 <khomyakov362@gmail.com>
2025-03-25 16:40:54 +0500

    Added a command for adding user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+NULLABLE = {'blank' : True, 'null' : True}
+
+class User(AbstractUser):
+    username = None
+    emeil = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=35, verbose_name='phone number', **NULLABLE)
+    telegram = models.CharField(max_length=150, verbose_name='telegram username', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}' 
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['id']
