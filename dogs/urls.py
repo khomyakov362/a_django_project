96e0da678eb6396613c8dcc795f7e68bcead8973
khomyakov362 <khomyakov362@gmail.com>
2025-03-27 21:15:04 +0500

    Writtenn some view functions for dogs and breeds

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from dogs.views import index
+from dogs.views import index, breeds_list, breed_dogs_list
 from dogs.apps import DogsConfig
 
 app_name = DogsConfig.name
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('breeds/', breeds_list, name='breeds'),
+    path('breeds/<int:pk>/dogs/', breed_dogs_list, name='breed_dogs')
 ]
 
--- dogs/views.py
+++ dogs/views.py
@@ -9,3 +9,19 @@ def index(request):
     }
 
     return render(request, 'dogs/index.html', context)
+
+def breeds_list(request):
+    context = {
+        'objects_list' : Breed.objects.all(),
+        'title' : 'Shelter - All Our Breeds'
+    }
+    return render(request, 'dogs/breeds.html', context)
+
+def breed_dogs_list(request, pk):
+    breed_item = Breed.objects.get(pk=pk)
+    context = {
+        'objects_list' : Dog.objects.filter(breed_id=pk),
+        'title' : f'Dogs of {breed_item.name} breed',
+        'breed_pk' : breed_item.pk
+    }
+    return render(request, 'dogs/dogs.html', context)
