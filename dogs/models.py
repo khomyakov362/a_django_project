e4819dbc6b07b498c2ec4b530a10127a8929f4b7
khomyakov362 <khomyakov362@gmail.com>
2025-03-27 20:07:05 +0500

    Added Dob model, added Pillow to requirements

--- dogs/models.py
+++ dogs/models.py
@@ -12,3 +12,16 @@ class Breed(models.Model):
     class Meta:
         verbose_name = 'breed'
         verbose_name_plural = 'breed'
+
+class Dog(models.Model):
+    name = models.CharField(max_length=150, verbose_name='dog_name')
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, verbose_name='breed')
+    photo = models.ImageField(upload_to='dogs/', **NULLABLE)
+    birth_date = models.DateField(**NULLABLE, verbose_name='birth_date')
+
+    def __str__(self):
+        return f'{self.name} ({self.breed})'
+    
+    class Meta:
+        verbose_name = 'dog'
+        verbose_name_plural = 'dogs'
