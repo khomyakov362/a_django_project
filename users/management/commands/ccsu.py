1b068572a93bb6076a6922bb3d3a1400ac2cca07
khomyakov362 <khomyakov362@gmail.com>
2025-04-20 11:52:40 +0500

    Added roles, migrated to new database, reworked base.html

--- users/management/commands/ccsu.py
+++ users/management/commands/ccsu.py
@@ -8,6 +8,7 @@ class Command(BaseCommand):
             email='admin@web.top',
             first_name='Admin',
             last_name='Admin',
+            role='admin',
             is_staff=True,
             is_superuser=True,
             is_active=True,
@@ -16,3 +17,32 @@ class Command(BaseCommand):
         admin_user.set_password('qwerty')
         admin_user.save()
         print('Admin created')
+    
+        moderator = User.objects.create(
+                email='mod@web.top',
+                first_name='Moderator',
+                last_name='Moderator',
+                role='moderator',
+                is_staff=True,
+                is_superuser=False,
+                is_active=True,
+            )
+        
+        moderator.set_password('querty')
+        moderator.save()
+        print('Moderator created')
+    
+        user = User.objects.create(
+                email='user@web.top',
+                first_name='User',
+                last_name='User',
+                role='user',
+                is_staff=False,
+                is_superuser=False,
+                is_active=True,
+            )
+        
+        user.set_password('querty')
+        user.save()
+        print('User created')
+    
--- users/models.py
+++ users/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 NULLABLE = {'blank' : True, 'null' : True}
+
+class UserRoles(models.TextChoices):
+    ADMIN = 'admin', _('admin')
+    MODERATOR = 'moderator', _('moderator')
+    USER = 'user', _('user')
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(verbose_name='Email', unique=True)
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.USER)
     first_name = models.CharField(max_length=150, verbose_name='First name', default='Anonymous')
     last_name = models.CharField(max_length=150, verbose_name='Last Name', default='Anonymous')
     phone = models.CharField(max_length=35, verbose_name='Phone Number', **NULLABLE)
