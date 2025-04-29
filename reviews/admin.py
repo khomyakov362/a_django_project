78be8b2993d86642de50a9bec26a89c415deab82
khomyakov362 <khomyakov362@gmail.com>
2025-04-29 15:23:14 +0500

    Added model and form for reviews

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import Review
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'dog', 'author', 'created', 'sign_of_review',)
+    ordering = ('created',)
+    list_filter = ('dog', 'author')
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
 
-# Create your models here.
+from users.models import NULLABLE
+from dogs.models import Dog
+
+class Review(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Title')
+    slug = models.SlugField(max_length=25, unique=True, db_index=True, verbose_name='URL')
+    content = models.TextField(verbose_name='Content')
+    created = models.DateTimeField(verbose_name='Created', auto_now_add=True)
+    sign_of_review = models.BooleanField(default=True, verbose_name='Sign of review')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Author')
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE, related_name='dogs', verbose_name='dog')
+
+    def __str__(self):
+        return str(self.title)
+
+    def get_absolute_url(self):
+        return reverse('reviews:review_detail', kwargs={'slug' : self.slug})
+
+    class Meta:
+        verbose_name = 'review'
+        verbose_name_plural = 'reviews'
