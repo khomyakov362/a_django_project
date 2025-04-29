2bd5cf7c359ca4c9ecfd9d37ad525b8c45efcfc4
khomyakov362 <khomyakov362@gmail.com>
2025-04-29 16:12:08 +0500

    Added some views for reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from reviews.apps import ReviewsConfig
+from reviews import views
 
 app_name = ReviewsConfig.name
 
 urlpatterns = [
+    path('', views.ReviewListView.as_view(), name='review_list'),
+    path('deactivated/', views.ReviewDeactivatedListView.as_view(), name='review_deactivated_list'),
 
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.exceptions import PermissionDenied
+from redis.commands.search.querystring import querystring
 
-# Create your views here.
+from reviews.models import Review
+from users.models import User
+from reviews.forms import ReviewForm
+
+class ReviewListView(generic.ListView):
+    model = Review
+    extra_context = {
+        'title' : 'All Reviews'
+    }
+    template_name = 'reviews/reviews.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(sign_of_review=True)
+        return queryset
+
+class ReviewDeactivatedListView(generic.ListView):
+    model = Review
+    extra_context = {
+        'title' : 'Non-Active Reviews'
+    }
+    template_name = 'reviews/reviews.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(sign_of_review=False)
+        return queryset
