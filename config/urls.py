920ab3b6a7d26c24212f5c9af81cbcc0805e783f
khomyakov362 <khomyakov362@gmail.com>
2025-04-29 15:09:23 +0500

    Added reviews app

--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dogs.urls', namespace='dogs')),
-    path('users/', include('users.urls', namespace='users'))
+    path('users/', include('users.urls', namespace='users')),
+    path('reviews/', include('reviews.urls', namespace='reviews'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
